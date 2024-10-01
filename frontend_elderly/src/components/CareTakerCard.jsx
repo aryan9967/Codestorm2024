e99d3fb2f5658c643530877c16271defce5dc347
Ayush Sharma <ayush.s.sharma04@gmail.com>
2024-10-01 10:14:29 +0530

    Om Changes

--- frontend_elderly/src/components/CareTakerCard.jsx
+++ frontend_elderly/src/components/CareTakerCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Mail, Phone, DollarSign, BriefcaseIcon, Heart } from 'lucide-react';
+import { Mail, Phone, DollarSign, BriefcaseIcon, Heart, IndianRupee } from 'lucide-react';
 import caretakerprofile1 from '../assets/caretaker_profile12.jpg'
 
 export default function CareTakerCard({ caregiver }) {
@@ -32,8 +32,8 @@ export default function CareTakerCard({ caregiver }) {
                             <span className="text-gray-600 text-xs">{caregiver?.experience} years experience</span>
                         </div>
                         <div className="flex items-center">
-                            <DollarSign className="w-5 h-5 mr-2 text-purple-500" />
-                            <span className="text-gray-600 text-xs">${caregiver?.chargesPerDay}/day</span>
+                            <IndianRupee className="w-5 h-5 mr-2 text-purple-500" />
+                            <span className="text-gray-600 text-xs">₹{caregiver?.chargesPerDay}/day</span>
                         </div>
                     </div>
                 </div>
--- frontend_elderly/src/components/DoctorCard.jsx
+++ frontend_elderly/src/components/DoctorCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Phone, Calendar, Mail, DollarSign, BriefcaseIcon } from 'lucide-react';
+import { Phone, Calendar, Mail, DollarSign, BriefcaseIcon, IndianRupee } from 'lucide-react';
 import doctor_profile1 from '../assets/doctor_profile14.jpg';
 
 const DoctorProfileCard = ({ doctor }) => {
@@ -31,8 +31,8 @@ const DoctorProfileCard = ({ doctor }) => {
                             <span className="text-gray-600 text-xs">{doctor?.experience} years experience</span>
                         </div>
                         <div className="flex items-center">
-                            <DollarSign className="w-5 h-5 mr-2 text-purple-500" />
-                            <span className="text-gray-600 text-xs">${doctor?.chargesPerDay}/day</span>
+                            <IndianRupee className="w-5 h-5 mr-2 text-purple-500" />
+                            <span className="text-gray-600 text-xs">₹{doctor?.chargesPerDay}/day</span>
                         </div>
                     </div>
                 </div>
